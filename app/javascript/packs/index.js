8f9e471e27226d62549e1ed9bc6462a81a804d98
zheta88 <zytamayo@misena.edu.co>
2019-11-12 14:07:41 -0500

    cambios para renderizar react, #pendiente, no funciona aún

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import "bootstrap/dist/css/bootstrap.css"
+import Home from "../componentes/Home/Home"
 
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Home />,
+    <Home/>,
     document.body.appendChild(document.createElement('div')),
   )
 })
