23dcdfda7e0e8e083af09fb6fdb7faa04e03664f
zheta88 <zytamayo@misena.edu.co>
2019-11-12 15:04:03 -0500

    se reinstaló paquete styled-components-jumbotron

--- app/javascript/componentes/Home/Home.js
+++ app/javascript/componentes/Home/Home.js
@@ -2,15 +2,7 @@ import React, {Component} from "react"
 import Jumbotron from "./Jumbotron"
 
 class Home extends Component{
-    constructor(){
-        super()
-        this.state={
-            modules: [
-               {id: 1, title:"", description:"", active:false}
-            ]
-        }
-
-    }
+   
     render()  {
         return(
             <Jumbotron/>               
